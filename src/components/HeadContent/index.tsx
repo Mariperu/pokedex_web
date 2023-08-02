49ec023002d713a522c347f3cf3b39032af1f4de
Maritza Rodriguez <maritzarg2020@gmail.com>
2023-08-02 16:02:21 -0500

    feat: firts commit

---
+++ src/components/HeadContent/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+
+type Props = {
+  title: string;
+  description: string;
+  og_description: string;
+  keywords: string;
+};
+
+export function HeadContent({
+  title,
+  description,
+  og_description,
+  keywords,
+}: Props) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="name" content="Pokedex" />
+        <meta name="description" content={description} />
+        <meta property="og:title" content="Pokedex" />
+        <meta property="og:description" content={og_description} />
+        {/* <meta property="og:url" content="https://vercel.com/" /> */}
+        <meta property="og:type" content="website" />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content="Maritza Rodriguez" />
+      </Head>
+    </>
+  );
+}
---
+++ src/layouts/BasicLayout/index.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { HeadContent } from "./../../components";
+
+type Props = {
+  children: ReactNode;
+  title: string;
+  description: string;
+  og_description: string;
+  keywords: string;
+};
+
+export const BasicLayout = (props: Props) => {
+  return (
+    <div className="basicLayout">
+      <HeadContent
+        title={props.title}
+        description={props.description}
+        og_description={props.og_description}
+        keywords={props.keywords}
+      />
+      <main>{props.children}</main>
+    </div>
+  );
+};

01feb0528768e0cf53e142073869a729721f97cc
Maritza Rodriguez <maritzarg2020@gmail.com>
2023-08-08 15:13:03 -0500

    constants for colors

---
+++ src/utils/constants.ts
@@ -0,0 +1,40 @@
+export const API_HOST = "https://pokeapi.co/api/v2";
+
+export const POKEMON_TYPE_COLORS: any = {
+  normal: "#A8A878",
+  fighting: "#C03028",
+  flying: "#A890F0",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  rock: "#B8A038",
+  bug: "#A8B820",
+  ghost: "#705898",
+  steel: "#B8B8D0",
+  fire: "#FA6C6C",
+  water: "#6890F0",
+  grass: "#48CFB2",
+  electric: "#FFCE4B",
+  psychic: "#F85888",
+  ice: "#98D8D8",
+  dragon: "#7038F8",
+  dark: "#705848",
+  fairy: "#EE99AC",
+};
+
+export const POKEMON_STATS_COLORS: any = [
+  "rgba(255, 99, 132, 0.5)",
+  "rgba(54, 162, 235, 0.7)",
+  "rgba(255, 206, 86, 0.7)",
+  "rgba(75, 192, 192, 0.7)",
+  "rgba(153, 102, 255, 0.7)",
+  "rgba(255, 159, 64, 0.7)",
+];
+
+export const POKEMON_STATS_PASTEL_COLORS: any = [
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(153, 102, 255, 0.2)",
+  "rgba(255, 159, 64, 0.2)",
+];
